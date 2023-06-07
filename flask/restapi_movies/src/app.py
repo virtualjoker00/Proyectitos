e8a41a9e5612476d452c9359104680252f2f8207
Julián Zúñiga <108155631+virtualjoker00@users.noreply.github.com>
2023-06-07 00:31:58 -0400

    flask ejercicios

--- flask/restapi_movies/src/app.py
+++ flask/restapi_movies/src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import config 
+from flask_cors import CORS
 from routes import Movie
 
 app = Flask(__name__)
 
+#CORS(app,resources={"*":{"origins":"http://localhost:9300"}})
 def page_not_found(error):
     return "<h1>Not found page</h1>",404
 
